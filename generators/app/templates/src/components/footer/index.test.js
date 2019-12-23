03e9037b28e3b900c86b073604605c3b300f7bc9
Simon Hudson <simon.hudson@experian.com>
2019-12-23 08:05:38 +0000

    update tests

--- generators/app/templates/src/components/footer/index.test.js
+++ generators/app/templates/src/components/footer/index.test.js
@@ -4,19 +4,22 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import { expect } from 'chai';
 import Footer from './index';
+const cloneDeep = require('lodash/cloneDeep');
 
+const baseProps = {};
 describe('Footer', () => {
-    
+
     let objectUnderTest;
 	const selector = `[data-test="footer"]`;
-    
+
     it('should render as expected', () => {
-        initialise();
+        const props = cloneDeep(baseProps);
+        initialise(props);
         expect(objectUnderTest.html()).to.not.be.null;
         expect(objectUnderTest.exists(selector)).to.be.true;
         expect(objectUnderTest.find(selector).length).to.equal(1);
     });
 
-    const initialise = () => objectUnderTest = shallow(<Footer />);
-    
+    const initialise = props => objectUnderTest = shallow(<Footer {...props} />);
+
 });
--- generators/app/templates/src/components/header/index.test.js
+++ generators/app/templates/src/components/header/index.test.js
@@ -4,19 +4,23 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import { expect } from 'chai';
 import Header from './index';
+const cloneDeep = require('lodash/cloneDeep');
+
+const baseProps = {};
 
 describe('Header', () => {
-    
+
     let objectUnderTest;
 	const selector = `[data-test="header"]`;
-    
+
     it('should render as expected', () => {
-        initialise();
+        const props = cloneDeep(baseProps);
+        initialise(props);
         expect(objectUnderTest.html()).to.not.be.null;
         expect(objectUnderTest.exists(selector)).to.be.true;
         expect(objectUnderTest.find(selector).length).to.equal(1);
     });
 
-    const initialise = () => objectUnderTest = shallow(<Header />);
-    
+    const initialise = props => objectUnderTest = shallow(<Header {...props} />);
+
 });
