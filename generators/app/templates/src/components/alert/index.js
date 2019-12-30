01308511d2ae5c50303901379827aab3749c6d6a
Simon Hudson <simon.hudson@experian.com>
2019-12-30 09:52:06 +0000

    update alert

--- generators/app/templates/src/components/alert/index.js
+++ generators/app/templates/src/components/alert/index.js
@@ -2,15 +2,26 @@
 
 import React from 'react';
 import './css/styles.scss';
+import Icon from '~/components/icon';
 
 const VALID_TYPES = ['info', 'warning', 'error', 'success'];
+
+const ICONS = {
+	'info': 'info-circle',
+	'warning': 'exclamation-triangle',
+	'error': 'times',
+	'success': 'check-circle'
+};
 
 const Alert = props => {
 
 	if (!props || !VALID_TYPES.includes(props.type)) return null;
 
 	return (
-        <div className={`alert alert--${props.type}`} data-test="alert">
+        <div className={`alert alert--${props.type} ${props.hasIcon ? 'alert--has-icon' : ''}`} data-test="alert">
+			{props.hasIcon && (
+				<Icon type={ICONS[props.type]} />
+			)}
             {props.children}
         </div>
     );
