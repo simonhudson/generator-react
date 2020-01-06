9ebcd04287e1e77d86f1af5e487795f0bc4acdb2
Simon Hudson <simon.hudson@experian.com>
2020-01-06 08:16:35 +0000

    accordion tests

--- generators/app/templates/src/components/accordion/index.js
+++ generators/app/templates/src/components/accordion/index.js
@@ -5,12 +5,12 @@ import './css/styles.scss';
 
 const Accordion = props => {
 
-	if (!props.triggerText) return null;
+	if (!props.summaryText) return null;
 
 	return (
         <details className="accordion" data-test="accordion">
-            <summary className="accordion__summary" data-test="accordion__summary">{props.triggerText}</summary>
-            <div className="accordion__content">
+            <summary className="accordion__summary" data-test="accordion__summary">{props.summaryText}</summary>
+            <div className="accordion__content" data-test="accordion__content">
 				{props.children}
 			</div>
 		</details>
---
+++ generators/app/templates/src/components/accordion/index.test.js
@@ -0,0 +1,49 @@
+'use strict';
+
+import React from 'react';
+import { mount } from 'enzyme';
+import { expect } from 'chai';
+import Accordion from './index';
+const cloneDeep = require('lodash/cloneDeep');
+import { assertElementExists, assertElementDoesNotExist } from '&/tests/utilities';
+
+const baseProps = {
+    summaryText: 'Find out more information'
+};
+
+describe('Accordion', () => {
+
+    let objectUnderTest;
+	const selector = `[data-test="accordion"]`;
+    const summary = `[data-test="accordion__summary"]`;
+    const content = `[data-test="accordion__content"]`;
+
+    it('should return null when no summaryText prop passed', () => {
+        const props = cloneDeep(baseProps);
+        delete props.summaryText;
+        initialise(props);
+        assertElementDoesNotExist(objectUnderTest, selector);
+    });
+
+    it('should render as expected', () => {
+        const props = cloneDeep(baseProps);
+        initialise(props);
+        assertElementExists(objectUnderTest, selector);
+    });
+
+    it('should render with correct summary text', () => {
+        const props = cloneDeep(baseProps);
+        initialise(props);
+        assertElementExists(objectUnderTest, summary);
+        expect(objectUnderTest.find(summary).text()).to.equal('Find out more information');
+    });
+
+    it('should render with content element', () => {
+        const props = cloneDeep(baseProps);
+        initialise(props);
+        assertElementExists(objectUnderTest, content);
+    });
+
+    const initialise = props => objectUnderTest = mount(<Accordion {...props} />);
+
+});
